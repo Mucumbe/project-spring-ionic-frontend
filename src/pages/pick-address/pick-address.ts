973691efcc45ab6a92571ec83653e828aded57eb
Mucumbe <Mucumbeblandino@gmail.com>
2020-06-06 18:04:02 +0200

    Tela de escolha de pagamento

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -52,7 +52,7 @@ export class PickAddressPage {
   }
   nextPage(item: EnderecoDTO){
     this.pedido.enderecodeEntrega={id: item.id};
-    console.log(this.pedido);
+    this.navCtrl.push('PaymentPage',{pedido: this.pedido});
   }
 
 }
