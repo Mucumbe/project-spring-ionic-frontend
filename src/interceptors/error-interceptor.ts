eec0ba38550ff0aacbdf0f54eb449e44b20fba62
Mucumbe <Mucumbeblandino@gmail.com>
2020-06-09 22:08:13 +0200

    infinite scroll

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -13,7 +13,6 @@ export class ErrorInterceptor implements HttpInterceptor {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log("passou interceptor")
         return next.handle(req)
         .catch((error, caught) => {
 
--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -11,7 +11,8 @@ import { API_CONFIG } from '../../config/api.config';
   templateUrl: 'produtos.html',
 })
 export class ProdutosPage {
-  items : ProdutoDTO[];
+  items : ProdutoDTO[]=[];
+  page:number=0;
 
   constructor(
     public navCtrl: NavController, 
@@ -22,25 +23,29 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
-    this.loadDate();
+    this.loadData();
   }
 
-  loadDate(){
+  loadData(){
     let categoria_id=this.navParams.get('categoria_id');
     let loader=this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id)
+    this.produtoService.findByCategoria(categoria_id,this.page,10)
     .subscribe(response=> {
-      this.items=response['content'];
+      let start=this.items.length;
+      this.items=this.items.concat(response['content']);
+      let end= this.items.length-1;
+      console.log(this.page);
+      console.log(this.items);
       loader.dismiss();
-      this.loadImageUrls();
+      this.loadImageUrls(start,end);
     },
     error=>{
       loader.dismiss();
     });
   }
 
-  loadImageUrls() {
-    for(var i=0;i<this.items.length;i++){
+  loadImageUrls(start:number,end:number) {
+    for(var i=start;i<=end;i++){
       let item=this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
       .subscribe(response=>{
@@ -61,10 +66,19 @@ export class ProdutosPage {
     return loader;
   }
 
-  doRefresh(event) {
-    this.loadDate();
+  doRefresh(refresher) {
+    this.page=0
+    this.items=[]
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
+  }
+  doInfinite(event) {
+    this.page++;
+    this.loadData();
     setTimeout(() => {
       event.complete();
-    }, 1000);
+    },1000);
   }
 }
